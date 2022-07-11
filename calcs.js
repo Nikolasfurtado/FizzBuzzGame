495afd2b66fef850fadfd7adf84b319e93ad363a
Nikolas Furtado <nikolasfurtado@gmail.com>
2022-07-10 23:06:33 -0400

    calc working for fizz

--- calcs.js
+++ calcs.js
@@ -1,29 +1,42 @@
-var check = document.getElementById("fizzcount");
-var counter = 1;
-var storage = [];
+    var check = document.getElementById("fizzcount");
 
+    var storage = [];
+    var counter = 0;
 
-function fizzbuzz() {
-    while (counter <= 100) {
+    function calcfizz() {
+        if (counter < 100) {
 
-        var fizz = counter % 3;
-        var buzz = counter % 5;
+            let fizz = counter % 3;
+            let buzz = counter % 5;
 
-        if (fizz === 0 && buzz === 0) {
-            let addfizz = '<spam>Fizz-buzz!, </spam>';
-            fizzcount.insertAdjacentHTML("beforeend", addfizz);
-        } else if (fizz === 0) {
-            let addbuzz = '<spam>Fizz!, </spam>';
-            fizzcount.insertAdjacentHTML("beforeend", addbuzz);
-        } else if (buzz === 0) {
-            let addfbuzz = '<spam>Buzz!, </spam>';
-            fizzcount.insertAdjacentHTML("beforeend", addfbuzz);
-        } else {
-            let addnormal = counter + ', ';
-            fizzcount.insertAdjacentHTML("beforeend", addnormal);
+            if (fizz === 0 && buzz === 0) {
+                let addfizz = '<spam>Fizz-buzz!, </spam>';
+                fizzcount.insertAdjacentHTML("beforeend", addfizz);
+            } else if (fizz === 0) {
+                let addbuzz = '<spam>Fizz!, </spam>';
+                fizzcount.insertAdjacentHTML("beforeend", addbuzz);
+            } else if (buzz === 0) {
+                let addfbuzz = '<spam>Buzz!, </spam>';
+                fizzcount.insertAdjacentHTML("beforeend", addfbuzz);
+            } else {
+                let addnormal = counter + ', ';
+                fizzcount.insertAdjacentHTML("beforeend", addnormal);
+            }
+            storage.push(counter);
+
+            counter++;
         }
-        storage.push(counter);
-        counter += 1;
+
     }
 
-}
+
+    function fizzbuzz() {
+        setInterval(() => {
+            calcfizz()
+        }, 100);
+
+
+
+
+
+    }
